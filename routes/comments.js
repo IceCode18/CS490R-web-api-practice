4d157531a6e7ec673c439d21854895d4a8843936
Kaiser19 <jtiano19@go.byuh.edu>
2020-05-20 23:22:49 -1000

    Add post request route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -6,6 +6,18 @@ router.get("/", (req, res) => {
     res.send("You are on home directory."); 
 });
 
-
+router.post("/", async (req, res) => {
+   const comment = new Comment({
+       user_id: req.body.user_id,
+       post_id: req.body.post_id,
+       body: req.body.body
+   });
+   try{
+       const savedComment = await comment.save();
+       res.json(savedComment);
+   }catch(error){
+       res.json({ message: error });
+   }
+});
 
 module.exports = router;
