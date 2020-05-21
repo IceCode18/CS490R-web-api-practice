1816f87dea137e4f86c72bfc54249b4335182095
Kaiser19 <jtiano19@go.byuh.edu>
2020-05-20 21:28:14 -1000

    Add mongoose and set up DB connection

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express();
 
+// DB connection variables
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://projectTester:projecttestersupersecret@projecttest-pgisb.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.get("/", (req, res) => {
     res.send("You are on home directory."); 
 });
 
 app.listen("3000", (req, res) => {
     console.log("Now listening to port 3000");
+});
+
+
+
+client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Connected")
+  client.close();
 });
