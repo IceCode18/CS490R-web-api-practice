9b1e9647fb9a5d383cb92278b706f725024fa62b
Kaiser19 <jtiano19@go.byuh.edu>
2020-05-27 19:05:22 -1000

    Comment out mongoDB and mongoose codes

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 // Express variables
 const express = require('express')
-const app = express();
 const bodyParser = require('body-parser')
+const commentsRoute = require('./routes/comments');
+const app = express();
 
 // Use body-parser middleware
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use('/comments', commentsRoute); // Routes import
 
 // DB connection variables
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true, useUnifiedTopology: true  });
-
-// Routes import
-const commentsRoute = require('./routes/comments');
-app.use('/comments', commentsRoute);
-
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true, useUnifiedTopology: true  });
 
 // Connect to DB
-mongoose.connection
-.once("open",() => console.log("Connected"))
-.on("error", error => {
-  console.log(error)
-});
-
-app.listen("27017", (req, res) => {
+// mongoose.connection
+// .once("open",() => console.log("Connected"))
+// .on("error", error => {
+//   console.log(error)
+// });
+//27017
+app.listen("3000", (req, res) => {
     console.log("Now listening to port 3000");
 });
