a8ea3999638bbcb01dc5423780f050f26dea6116
Kaiser19 <jtiano19@go.byuh.edu>
2020-05-27 22:06:44 -1000

    Modify mongoose connection to database

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 // Express variables
 const express = require('express')
 const bodyParser = require('body-parser')
-const commentsRoute = require('./routes/comments');
-const app = express();
+const commentsRoute = require('./routes/comments')
+const mongoose = require("mongoose")
 
-// Use body-parser middleware
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use('/comments', commentsRoute); // Routes import
-
-// DB connection variables
-// const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true, useUnifiedTopology: true  });
 // Connect to DB
+
+mongoose.connect("mongodb://mongo:27017/test", {useNewUrlParser: true, useUnifiedTopology: true  })
+        .then( () => {
+            const app = express()
+            app.use(bodyParser.urlencoded({extended: true}))
+            app.use(bodyParser.json())
+            app.use('/comments', commentsRoute); // Routes import
+
+            app.listen("3000", (req, res) => {
+                console.log("Now listening to port 3000...");
+            })
+        })
+
+
 // mongoose.connection
 // .once("open",() => console.log("Connected"))
 // .on("error", error => {
 //   console.log(error)
 // });
 //27017
-app.listen("3000", (req, res) => {
-    console.log("Now listening to port 3000");
-});
+
