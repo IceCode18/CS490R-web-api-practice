ef95e5f35ad127b02550e036ac0d8964296ae883
Kaiser19 <jtiano19@go.byuh.edu>
2020-06-19 17:00:11 -1000

    Add Vue module to project

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // Express variables
-const express = require('express')
-const bodyParser = require('body-parser')
-const passport =require('passport')
-const routes = require('./routes')
-const mongoose = require("mongoose")
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport =require('passport');
+const routes = require('./routes');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 // Connect to DB
 mongoose.connect("mongodb://mongo:27017/test", {useNewUrlParser: true, useUnifiedTopology: true  })
         .then( () => {
-            const app = express()
-            app.use(bodyParser.urlencoded({extended: true}))
-            app.use(bodyParser.json())
-            app.use('/api/auth', routes.auth)
+            const app = express();
+            app.use(bodyParser.urlencoded({extended: true}));
+            app.use(bodyParser.json());
+            app.use(cors());
+            app.use('/api/auth', routes.auth);
             app.use('/api', passport.authenticate('jwt', { session: false }), routes.comments); // Routes import
             app.use('/api', passport.authenticate('jwt', { session: false }), routes.users);
             
