11d573c9c05fdf792564c893f6703d2e999bf12b
Kaiser19 <jtiano19@go.byuh.edu>
2020-06-19 20:29:31 -1000

    Add login request handler

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -20,6 +20,24 @@ class AuthService {
                 });
         });
     }
+
+    static login(user){
+        return new Promise((resolve, reject) => {
+            axios
+                .post(API_URL + "/login", {
+                    email: user.email,
+                    password: user.password,
+                })
+                .then((res) => {
+                    console.log(res);
+                    resolve(res.data.token);
+                })
+                .catch((err) => {
+                    //console.log("Service returned failure");
+                    reject(err);
+                });
+        });
+    }
 }
 
 export default AuthService;
