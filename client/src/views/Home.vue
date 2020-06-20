1b8c988057ddf8a95d8aebc687d1afbd6cb5c6fd
Kaiser19 <jtiano19@go.byuh.edu>
2020-06-19 17:36:46 -1000

    Add Login Page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h2>Home</h2>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<!-- <script>
+// // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+// export default {
+//   name: "Home",
+//   components: {
+//     HelloWorld
+//   }
+// };
+// </script>
+-->
