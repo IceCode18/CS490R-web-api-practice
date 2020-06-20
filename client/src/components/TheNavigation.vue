80204197225dfbcf3ca8ef9e93c955e65404c6c6
Kaiser19 <jtiano19@go.byuh.edu>
2020-06-19 17:27:44 -1000

    Add Navigation template

---
+++ client/src/components/TheNavigation.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav id="nav">
+        <p class="logo">Simple Express/Vue Blog</p>
+        <ul class="nav-links">
+            <li>
+                <router-link :to="{ name: 'Home'}" class="links">Home</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'About'}" class="links">About</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'SignUp'}" class="links">SignUp</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'Login'}" class="links">Login</router-link>
+            </li>
+        </ul>
+</template>
+<style scoped>
+    #nav {
+        display: flex;
+        align-items: center;
+        position: sticky;
+        top: 0;
+        background-color: white;
+        border: 1px solid gray;
+        z-index: 1;
+    }
+
+    #nav ul{
+        list-style-type: none;
+    }
+
+    #nav a {
+        text-decoration: none;
+        font-weight: bold;
+        color: #2c3e50;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .nav-links {
+        display: flex;
+    }
+
+    .links{
+        padding-right: 20px;
+        list-style: none;
+    }
+
+    .links:hover{
+        text-decoration: underline;
+    }
+
+    .logo{
+        font-size: 20px;
+        color: blue;
+        font-weight: bold;
+    }
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -13,16 +13,26 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/Signup.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+  mode: "history",
+  routes: routes,
 });
 
 export default router;
